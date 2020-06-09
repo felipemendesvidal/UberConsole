01ed6e478f04225c2314f90da4499267021219b1
felipe vidal mendes <contatofelipevidalmendes@gmail.com>
2020-06-09 11:25:32 -0300

    Diretorio do programa criado Iniciado e Vazio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uberConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
