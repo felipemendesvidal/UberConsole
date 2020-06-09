a7f2dcce49353fe3a2944cfbde0294fc391d12e1
felipe vidal mendes <contatofelipevidalmendes@gmail.com>
2020-06-09 12:25:36 -0300

    classe usuario criado, atributos e metodos feitos e testados, parte do login funcionando

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace uberConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Usuario user = new Usuario();
+            Console.WriteLine("Usuario: ");
+
+            //variavel para armazenar valor e fazer a comparação com os espaços já existentes na classe usuario
+            string login = Console.ReadLine();
+            Console.WriteLine("Digite sua senha: ");
+            string senha = Console.ReadLine();
+
+            //chamando função para fazer validação
+            //esse valores armazenados nas variaveis criadas, serão atribuidos  as variaveis de apois dessa fnção
+            user.Login(login, senha);
+            
+            if(user.TokenLogin != "" && user.TokenLogin != null){
+                Console.WriteLine("Acesso liberado! Usuário logado");
+                Console.WriteLine(user.TokenLogin);
+            }else{
+                Console.WriteLine("deslogado");
+            }
+
         }
     }
 }
--- Usuario.cs
+++ Usuario.cs
@@ -2,6 +2,29 @@ namespace uberConsole
 {
     public class Usuario
     {
-        
+        public string Nome {get; set;}
+        public string login = "felipe@gmail.com";
+        public string senha = "1234";
+        public string Foto {get; set;}
+        public string LocalizacaoAtual{get; set;}
+        public string TokenLogin{get; set;}
+
+        /// <summary>
+        /// Metodo que valida login
+        /// verifica if(this.login == login && this.senha == senha)
+        /// </summary>
+        /// <param name="loginAtributo">pega dados</param>
+        /// <param name="senhaAtributo">pega dados</param>
+        /// <returns>retorna em = TokenLogin = "senha ok";</returns>
+        public bool Login(string loginAtributo, string senhaAtributo){
+            if(this.login == loginAtributo && this.senha == senhaAtributo){
+                TokenLogin = "senha ok";
+                return true;
+            }
+            return false;
+        }
+        public void Logout(){
+            TokenLogin = "";
+        }
     }
 }
